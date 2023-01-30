ff0846e840354f6ff9279d3ff6f40112e4d7015c
Sophie <87262182+iSophes@users.noreply.github.com>
2023-01-30 22:31:06 +0000

    build removal

--- build/assets/js/7969c432.0ddd91a8.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[13],{63891:s=>{s.exports=JSON.parse('{"sourceUrl":"https://github.com/iSophes/MathPlus/blob/dev","baseUrl":"/MathPlus/","classOrder":[],"apiCategories":[]}')}}]);
